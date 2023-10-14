616af50f55da4cc0a9edf622f8281d186014c401
basil daboor <basildaboor12@gmail.com>
2023-10-14 16:54:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(title => {
+
+      const words = title.split(' ');
+
+
+      const titleCasedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+
+    return titleCasedWords.join(' ');
+  });
+};
